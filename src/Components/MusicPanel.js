4b2c4ac734924c2b10ccdb92d468b54b145bb312
JRob <72467031+Jonathan-84@users.noreply.github.com>
2021-06-12 09:02:30 -0400

    updated the music panel to enable drop down choices

--- src/Components/MusicPanel.js
+++ src/Components/MusicPanel.js
@@ -1,4 +1,72 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+
+// Look into the ability to integrate the playe, rather than just the iframe. Iframe only gives sample of song,
+//want the ability to play on-going, that's the purpose of keeping it the sameo on left panel
+
+class Music extends Component {
+ 
+
+  constructor(props) {
+    super(props);
+    this.state = {value: 'https://open.spotify.com/embed/playlist/792i0xmXcx6A9R3dYr3koO?theme=0'
+  };
+
+
+    this.handleChange = this.handleChange.bind(this);
+
+
+  }
+
+ 
+  
+  handleChange(event) {
+    this.setState({value: event.target.value
+    });
+  
+  }
+
+  render() {
+
+
+    return (
+      <>
+      <br></br>
+      <form className='row justify-content-around d-flex flex-column flex-lg-row align-content-center'>
+        <label className= 'music-color'>  Space Radio </label>
+          <select value={this.state.value} onChange={this.handleChange} className="music-color">
+          <option value="https://open.spotify.com/embed/playlist/792i0xmXcx6A9R3dYr3koO?theme=0">Listen In Space</option>
+            <option value='https://open.spotify.com/embed/album/5qdsq5YryqAynT65vpaumI'>Space Jam</option>
+            <option value="60">2 Months</option>
+            <option value="90">3 Months</option>
+            <option value="180">6 Months</option>
+            <option value="270">9 Months</option>
+          </select>
+     
+      </form>
+      <br></br>
+      <div className=" center-music">
+ <iframe src={this.state.value}
+     width="100%" height="400" 
+     frameBorder="0" title='Spotify'allowtransparency="true" allow="encrypted-media">Listen In Space</iframe>
+        </div>
+  <br>
+  </br>
+  <br></br>
+  <br></br>
+  
+
+            
+      </>
+    );
+}
+}
+
+export default Music;
+    
+
+
+/*import React from 'react';
 
 
 
@@ -9,12 +77,13 @@ const Music = () => {
     return (
       <>
      
-        <iframe src="https://open.spotify.com/embed/playlist/792i0xmXcx6A9R3dYr3koO?theme=0" 
-        width="100%" height="450" frameBorder="0" allowtransparency="true" 
-        allow="encrypted-media" title="Spotify">Space Play List</iframe>
+     <iframe src="https://open.spotify.com/embed/playlist/792i0xmXcx6A9R3dYr3koO?theme=0" 
+     width="100%" height="500" 
+     frameBorder="0" title='Spotify'allowtransparency="true" allow="encrypted-media">Listen In Space</iframe>
 
       </>
     );
   }; 
   export default Music;
 
+*/
